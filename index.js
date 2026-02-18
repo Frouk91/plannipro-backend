5bf12ded9ce6a4f80f5b933a315c32fc00caa0ef
Redouane <f.redouane@quadient.com>
2026-02-18 17:21:43 +0100

    Fix variables DB

--- index.js
+++ index.js
@@ -5,7 +5,11 @@ const path = require('path');
 
 async function initDB() {
   const pool = new Pool({
-    connectionString: process.env.DATABASE_URL,
+    host: process.env.PGHOST,
+    port: process.env.PGPORT,
+    user: process.env.PGUSER,
+    password: process.env.PGPASSWORD,
+    database: process.env.PGDATABASE,
     ssl: { rejectUnauthorized: false }
   });
   try {
