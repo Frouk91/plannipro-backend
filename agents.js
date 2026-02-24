9fef3a1e9340502f277a5de1eddd82eff5e6759f
Redouane <f.redouane@quadient.com>
2026-02-24 12:32:59 +0100

    Fix: ./pool au lieu de ../pool

--- agents.js
+++ agents.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const db = require('../pool');
+const db = require('./pool');
 const bcrypt = require('bcrypt');
 
 // GET tous les agents
